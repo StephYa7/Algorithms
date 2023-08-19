5755c2b77fcab833b8b182a88f0a4e3e9e17675d
StephYa7 <1000p@mail.ru>
2023-08-20 04:01:20 +0700

    Initial commit +3sort(bubble, selection, insertion)

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,9 +2,7 @@
 public class BubbleSort {
     public static void main(String[] args) {
         int[] array = new int[]{45, 22, 11, 35, 67, 12, 81, 3, 57, 14, 8, 99, 19, 71, 13, 77};
-
         bubbleSort(array);
-
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
