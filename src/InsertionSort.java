1a81efa4082b3841215a67546c4d06401024c45f
StephYa7 <1000p@mail.ru>
2023-08-21 15:23:13 +0700

    +quickSort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,10 +3,11 @@ public class InsertionSort {
         int[] array = new int[]{45, 22, 11, 35, 67, 12, 81, 3, 57, 14, 8, 99, 19, 71, 13, 77};
         insertionSort(array);
 
-        for (int j:array) {
+        for (int j : array) {
             System.out.print(j + " ");
         }
     }
+
     public static void insertionSort(int[] array) {
 
         for (int i = 1; i < array.length; i++) {
@@ -15,7 +16,7 @@ public class InsertionSort {
 
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
-                j = j - 1;
+                j -= 1;
             }
             array[j + 1] = key;
         }
---
+++ src/QuickSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{45, 22, 11, 35, 67, 12, 81, 3, 57, 14, 8, 99, 19, 71, 13, 77};
+        quickSort(array, 0, array.length - 1);
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+    }
+
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = arr[high];
+            int i = low - 1;
+            for (int j = low; j < high; j++) {
+                if (arr[j] < pivot) {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            int partitionIndex = i + 1;
+            quickSort(arr, low, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, high);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
